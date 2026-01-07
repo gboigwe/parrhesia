0d8d6f4b50ce6bf0f9d2496d5fb121f375a38606
Gbolahan Akande <geakande@gmail.com>
2026-01-07 02:02:19 +0100

    Export reputation types and leaderboard utils

--- src/lib/reputation/index.ts
+++ src/lib/reputation/index.ts
@@ -17,7 +17,30 @@ export {
   calculateReputationChange,
 } from "./calculateReputation";
 
+export {
+  sortByReputation,
+  addRanks,
+  filterByCategory,
+  getTopN,
+  calculateAverageReputation,
+  getUserRank,
+  formatRank,
+  getRankChangeIcon,
+  calculatePercentile,
+} from "./leaderboardUtils";
+
 export type {
   ReputationUpdateParams,
   ReputationUpdateResponse,
 } from "./updateReputation";
+
+export type {
+  ReputationScore,
+  DebaterStats,
+  VoterStats,
+  LeaderboardEntry,
+  LeaderboardData,
+  ReputationTier,
+  ReputationBreakdown,
+  ReputationHistory,
+} from "./types";
