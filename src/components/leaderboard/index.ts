d3955ce103289f17ccf1a9274d4775eafbcaa413
Gbolahan Akande <geakande@gmail.com>
2026-01-07 02:03:32 +0100

    Export UserReputationCard component

--- src/components/leaderboard/index.ts
+++ src/components/leaderboard/index.ts
@@ -6,3 +6,4 @@ export { LeaderboardTable } from "./LeaderboardTable";
 export { ReputationStats } from "./ReputationStats";
 export { CategoryFilter } from "./CategoryFilter";
 export { RankBadge } from "./RankBadge";
+export { UserReputationCard } from "./UserReputationCard";
