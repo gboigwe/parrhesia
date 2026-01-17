8b3847dd5c00b76011b14c10b6f80f53204ebbdb
Gbolahan Akande <geakande@gmail.com>
2026-01-17 23:00:56 +0100

    Export moderation components and library

---
+++ src/lib/ai/moderator/index.ts
@@ -0,0 +1,9 @@
+/**
+ * AI Moderator Library Barrel Export
+ */
+
+export * from "./types";
+export * from "./constants";
+export * from "./moderatorAgent";
+export * from "./detectors";
+export * from "./utils";
