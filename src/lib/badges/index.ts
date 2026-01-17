cd5ce7d804074d6c58d788a20df0395b07935100
Gbolahan Akande <geakande@gmail.com>
2026-01-17 21:53:09 +0100

    Add badge library barrel exports

---
+++ src/lib/badges/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Badge System Barrel Exports
+ */
+
+export { BADGE_TYPES, BADGE_METADATA, BADGE_RARITY, RARITY_COLORS } from "./constants";
+export { detectNewBadges, checkBadgeProgress } from "./detectBadges";
+export { generateBadgeMetadata, getBadgeImageSVG } from "./metadataGenerator";
