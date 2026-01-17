7c795dcdaac088f1a5dee1e880d56b9ee75098c4
Gbolahan Akande <geakande@gmail.com>
2026-01-17 21:56:51 +0100

    Export badge types and utility functions

--- src/lib/badges/index.ts
+++ src/lib/badges/index.ts
@@ -5,3 +5,14 @@
 export { BADGE_TYPES, BADGE_METADATA, BADGE_RARITY, RARITY_COLORS } from "./constants";
 export { detectNewBadges, checkBadgeProgress } from "./detectBadges";
 export { generateBadgeMetadata, getBadgeImageSVG } from "./metadataGenerator";
+export {
+  getBadgeRarityColor,
+  sortBadgesByRarity,
+  groupBadgesByRarity,
+  calculateBadgeStats,
+  isDebaterBadge,
+  isVoterBadge,
+  getRarestBadge,
+} from "./utils";
+
+export type { Badge, BadgeProgress, BadgeMetadata, UserBadgeStats, BadgeEarnEvent } from "./types";
