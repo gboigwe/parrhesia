627dd7b03e5eef4c10fce1a52a3c2cc5f62a7c20
Gbolahan Akande <geakande@gmail.com>
2026-01-19 00:24:55 +0100

    Export prize components via barrel file

---
+++ src/components/prizes/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Prize Components - Barrel Exports
+ * Central export point for all prize-related UI components
+ */
+
+export { ClaimPrizeButton } from "./ClaimPrizeButton";
+export { ClaimStatus } from "./ClaimStatus";
+export { ClaimNotification } from "./ClaimNotification";
+export { PrizePoolDisplay } from "./PrizePoolDisplay";
+export { TransactionReceipt } from "./TransactionReceipt";
