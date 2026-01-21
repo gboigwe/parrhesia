9f133d158a1b67d5c153cd12d552cd6bd1203c12
Gbolahan Akande <geakande@gmail.com>
2026-01-21 16:22:20 +0100

    Create auth components barrel export

---
+++ src/components/auth/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Authentication Components
+ * Barrel export for auth UI components
+ */
+
+export { AuthGuard } from "./AuthGuard";
+export { AuthStatus } from "./AuthStatus";
+export { SignInButton } from "./SignInButton";
+export { BasenameRequired } from "./BasenameRequired";
+export { ConnectWallet } from "./ConnectWallet";
+export { WalletStatus } from "./WalletStatus";
+export { AuthError } from "./AuthError";
+export { OnboardingFlow } from "./OnboardingFlow";
