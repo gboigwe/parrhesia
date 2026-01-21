99e00141d8640c2d3fa6afbb68d94bc0770c4b20
Gbolahan Akande <geakande@gmail.com>
2026-01-21 16:22:03 +0100

    Create auth utilities barrel export

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Authentication Utilities
+ * Barrel export for all auth modules
+ */
+
+export * from "./siwe";
+export * from "./session";
+export * from "./middleware";
+export * from "./errors";
+export * from "./csrf";
+export * from "./logging";
+export * from "./refresh";
